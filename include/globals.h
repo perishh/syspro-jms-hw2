718f4d0e4bc4a1422389ea887130d02c9655f840
Peris <vlassis.periklis@gmail.com>
2026-05-26 02:29:36 +0300

    added read_all, write_all

--- include/globals.h
+++ include/globals.h
@@ -1,6 +1,28 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <stdio.h>
+
 #define PORT_IN_USER_RANGE(port) (port >= 1024 && port <= 49151)
+
+// TODO: Add doxygen doc
+
+/**
+ * @brief Writes exactly n bytes from buf to fd
+ * @param fd file descriptor to write to
+ * @param buf buffer to write from
+ * @param n number of bytes to write
+ * @return n on success, normal write(2) return value otherwise
+ */
+int write_all(int fd, char* buf, size_t n);
+
+/**
+ * @brief Reads exactly n bytes to buf from fd
+ * @param fd file descriptor to read from
+ * @param buf buffer to read into
+ * @param n number of bytes to read
+ * @return n on success, normal read(2) return value otherwise
+ */
+int read_all(int fd, char* buf, size_t n);
 
 #endif
---
+++ src/globals.c
@@ -0,0 +1,39 @@
+#include "globals.h"
+
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <unistd.h>
+
+int write_all(int fd, char* buf, size_t n) {
+  size_t total = 0;
+  while (total < n) {
+    ssize_t written = write(fd, buf, n - total);
+    if (written <= 0) {
+      if (errno == EINTR) {
+        // We can try again
+        continue;
+      }
+      return written;
+    }
+    total += written;
+    buf += written;
+  }
+  return n;
+}
+
+int read_all(int fd, char* buf, size_t n) {
+  while (n > 0) {
+    // TODO: Maybe use read_blocking?
+    ssize_t nread = read(fd, buf, n);
+    if (nread <= 0) {
+      if (errno == EINTR) {
+        // We can try again
+        continue;
+      }
+      return nread;
+    }
+    n -= nread;
+    buf += nread;
+  }
+  return n;
+}
